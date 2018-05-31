7cff9bc83f9dcdf43c690132184a84283edcd693
David Flemström <david.flemstrom@gmail.com>
2018-05-31 23:41:10 +0200

    Tweak stats query a bit

--- src/db/stats_query.sql
+++ src/db/stats_query.sql
@@ -1,8 +1,14 @@
 SELECT
   module_id,
-  min(moisture)        min_moisture,
-  max(moisture)        max_moisture,
-  last_value(moisture) last_moisture
+  min(moisture)
+  OVER w min_moisture,
+  max(moisture)
+  OVER w max_moisture,
+  last_value(moisture)
+  OVER w last_moisture
 FROM sample
-GROUP BY module_id
-ORDER BY created ASC;
+WINDOW w AS (
+  PARTITION BY module_id
+  ORDER BY created ASC
+  RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+);
