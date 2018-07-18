06cc112aa6caa725d1e62bd48b8516a3a1db86cf
David Flemström <david.flemstrom@gmail.com>
2018-07-19 00:46:07 +0200

    Clean up stuff

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,3 @@
-use std::char;
 use std::collections;
 use std::u8;
 
@@ -37,8 +36,6 @@ pub struct Plant {
 pub struct Moisture {
     #[serde(deserialize_with = "deserialize_moisture_channel")]
     pub channel: MoistureChannel,
-    #[serde(deserialize_with = "deserialize_distance")]
-    pub distance: f64,
     pub voltage_dry: f64,
     pub voltage_wet: f64,
     // in meters
@@ -85,50 +82,6 @@ where
         Err(serde::de::Error::invalid_value(
             serde::de::Unexpected::Str(&raw),
             &"a hexadecimal integer, a dash '-', and a decimal integer, like \"e8-3\"",
-        ))
-    }
-}
-
-fn deserialize_distance<'de, D>(deserializer: D) -> Result<f64, D::Error>
-where
-    D: serde::Deserializer<'de>,
-{
-    use std::str::FromStr;
-
-    let raw = <String as serde::Deserialize>::deserialize(deserializer)?;
-    if let Some(unit_start) = raw.find(char::is_alphabetic) {
-        let (number_str, unit_str) = raw.split_at(unit_start);
-        let number = f64::from_str(number_str).map_err(|e| {
-            serde::de::Error::invalid_value(
-                serde::de::Unexpected::Str(number_str),
-                &format!("a valid number: {}", e).as_str(),
-            )
-        })?;
-        let result = match unit_str {
-            "km" => number * 1000.0,
-            "hm" => number * 100.0,
-            "dam" => number * 10.0,
-            "m" => number,
-            "dm" => number / 10.0,
-            "cm" => number / 100.0,
-            "mm" => number / 1000.0,
-            "µm" => number / 1000.0,
-            "mum" => number / 1000.0,
-            "nm" => number / 1000_000.0,
-            "pm" => number / 1000_000_000.0,
-            other => {
-                return Err(
-                    serde::de::Error::invalid_value(
-                        serde::de::Unexpected::Str(other),
-                        &format!("a recognized metric unit (e.g. km, hm, dam, m, dm, cm, mm, µm, mum, nm, pm)").as_str(),
-                    ));
-            }
-        };
-        Ok(result)
-    } else {
-        Err(serde::de::Error::invalid_value(
-            serde::de::Unexpected::Str(&raw),
-            &"a number with a length unit, such as \"2.3cm\"",
         ))
     }
 }
